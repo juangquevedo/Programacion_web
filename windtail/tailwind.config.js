5d6e5b847747216a7cd56701cba5dbfb1136e37c
juangquevedo <62721948+juangquevedo@users.noreply.github.com>
2024-11-08 22:53:14 -0500

    Add files via upload

---
+++ windtail/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'), // Plugin de tipografía
+    require('@tailwindcss/forms'),      // Plugin de formularios
+  ],
+}

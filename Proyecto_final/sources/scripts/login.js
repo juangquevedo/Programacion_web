b379d5c681c6f168fc2c80bff7478fdb5dc6e22c
Juan Guillermo Quevedo <62721948+juangquevedo@users.noreply.github.com>
2024-11-22 20:41:00 -0500

    conexión entre el backed y frondend
    se habilito el login

---
+++ Proyecto_final/sources/scripts/login.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const loginButton = document.querySelector("button[type='button']");
+    const emailInput = document.getElementById("email");
+    const passwordInput = document.getElementById("password");
+
+    loginButton.addEventListener("click", async () => {
+        const email = emailInput.value.trim();
+        const password = passwordInput.value.trim();
+
+        if (!email || !password) {
+            alert("Por favor, completa todos los campos.");
+            return;
+        }
+
+        try {
+            const response = await fetch("http://localhost:3000/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ 
+                    email: email,
+                    contrasena: password
+                 }),
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                alert(`Error: ${errorData.message || "No se pudo iniciar sesión."}`);
+                return;
+            }
+
+            const data = await response.json();
+            alert("Inicio de sesión exitoso");
+            console.log("Usuario autenticado:", data);
+            sessionStorage.setItem('token', data.token);
+
+            // Redirigir a otra página después del inicio de sesión exitoso
+            window.location.href = "eventos.html";
+        } catch (error) {
+            console.error("Error al iniciar sesión:", error);
+            alert("Ocurrió un error al intentar iniciar sesión.");
+        }
+    });
+});
